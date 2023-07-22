3ffa90da3e710e87c49b3a80e3ba452218ba52ed
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-07-22 22:45:47 +0530

    Pattern3.java
    * * * * *
     * * * *
      * * *
       * *
        *

---
+++ Pattern3.java
@@ -0,0 +1,25 @@
+public class PatternPrint {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 0; i < rows; i++) {
+            // Print spaces
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print asterisks
+            for (int k = 0; k < rows - i; k++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+/* * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/

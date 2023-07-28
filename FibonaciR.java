d99d4d55e03bd82e8e648ee758aeeee3651ae610
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-07-28 18:34:53 +0530

    70. Climbing Stairs Using Recursion(LC)

---
+++ FibonaciR.java
@@ -0,0 +1,12 @@
+public class FibonaciR {
+    static int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+}

28fc262f356b727c2e4f90d455504da9a2877dc9
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-07-28 17:47:35 +0530

    Recursion

--- PowerOf2R.java
+++ PowerOf2R.java
@@ -1,16 +1,18 @@
 public class PowerOf2R {
     static boolean isPowerOf2(int n) {
+        if (n < 1) {
+            return false;
+        }
         if (n == 1) {
             return true;
         }
         if (n % 2 == 0) {
             return isPowerOf2(n / 2);
-        } else {
-            return false;
         }
+        return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPowerOf2(31));
+        System.out.println(isPowerOf2(0));
     }
 }

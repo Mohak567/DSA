16671643e503c76f73e9453b2109f3e5ae6e17c9
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-08-02 23:38:39 +0530

    GOOD QUESTION

--- Circular_winnerR.java
+++ Circular_winnerR.java
@@ -1,16 +1,13 @@
 public class Circular_winnerR {
     public static int ans(int n, int k) {
-        return helper(n, k) + 1;
-    }
-
-    public static int helper(int n, int k) {
         if (n == 1) {
             return 0;
         }
-        return (helper(n - 1, k) + k) % n;
+        return (ans(n - 1, k) + k) % n;
     }
 
     public static void main(String[] args) {
-        System.out.println(ans(5, 2));
+        int i = ans(5, 2) + 1;
+        System.out.println(i);
     }
 }

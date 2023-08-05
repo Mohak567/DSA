b429acc94e2e8bb5f02581069efd31a01c28aa2c
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-08-05 22:16:20 +0530

    Language Question

--- Chocolate_Distribution_Problem.java
+++ Chocolate_Distribution_Problem.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Chocolate_Distribution_Problem {
+    /*
+    Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum.(GFG)
+ */
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>();
         a.add(3);

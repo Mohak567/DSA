0552c19a0f9d55a31111d95e9e449cd873481337
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-08-14 19:20:49 +0530

    optimize

--- NFibonacci.java
+++ NFibonacci.java
@@ -1,18 +1,19 @@
 class NFibonacci {
-    static int nthFibonacci(int n){
-        if (n <= 1)
-            return n;
-            int MOD = 1000000007;
-            int[] fi = new int[n+1];
-            fi[0]=0;
-            fi[1]=1;
-            for(int i=2;i<=n;i++){
-                fi[i]=(fi[i-1]+fi[i-2])%MOD;
-            }
-       return fi[n];
+  static int nthFibonacci(int n) {
+    if (n <= 1)
+      return n;
+    int MOD = 1000000007;
+    int[] fi = new int[n + 1];
+    fi[0] = 0;
+    fi[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      fi[i] = (fi[i - 1] + fi[i - 2]) % MOD;
     }
-  public static void main(String[] args){
-    int n = 5;
-    System.out.println(nthFibonacci(n);
+    return fi[n];
+  }
+
+  public static void main(String[] args) {
+    int n = 100;
+    System.out.println(nthFibonacci(n));
   }
 }

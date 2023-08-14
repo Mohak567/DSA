4ed33133bb9dd1315b6440b9406b54aaae39aafd
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-08-14 19:16:25 +0530

    OPTIMIZED for recursion

---
+++ PermutationA2.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+
+public class PermutationA2 {
+    public static List<String> find_permutation(String S) {
+        String str = S;
+        if (str.length() == 0) {
+            List<String> list = new ArrayList<>();
+            list.add("");
+            return list;
+        }
+
+        char fc = str.charAt(0);
+        List<String> result = find_permutation(str.substring(1));
+        TreeSet<String> finalSet = new TreeSet<>();
+
+        for (String t : result) {
+            for (int i = 0; i <= t.length(); i++) {
+                StringBuilder sb = new StringBuilder(t);
+                sb.insert(i, fc);
+                finalSet.add(sb.toString());
+            }
+        }
+
+        return new ArrayList<>(finalSet);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find_permutation("acb"));
+    }
+}

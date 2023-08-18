c76c7f1782e0687000b9cc7ff921097a5c9925f6
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-08-18 23:54:56 +0530

    GOOD QUESTION(STATEMENT QUESTION)

--- Row_with_max_1s.java
+++ Row_with_max_1s.java
@@ -2,8 +2,8 @@ public class Row_with_max_1s {
     // Given a boolean 2D array of n x m dimensions where each row is sorted. Find
     // the 0-based index of the first row that has the maximum number of 1's.(GFG)
     public static int rowWithMax1s(int arr[][], int n, int m) {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {// column
+            for (int j = 0; j < n; j++) {// row
                 if (arr[j][i] == 1) {
                     return j;
                 }

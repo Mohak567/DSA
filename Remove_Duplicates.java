35acfee210ae68f8f24c0b9408e7f62f8fa66403
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-08-23 19:53:25 +0530

    good logic

--- Remove_Duplicates.java
+++ Remove_Duplicates.java
@@ -1,5 +1,11 @@
 import java.util.Arrays;
 
+/*Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k. *///Leet code 26
 public class Remove_Duplicates {// without using extra space
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 4, 5, 4, 6, 8, 9, 7, 3 };

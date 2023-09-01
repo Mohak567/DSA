b8939e606733fbc679928142348f847df15f3280
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-09-01 20:55:36 +0530

    ok ok question

--- Divisible_By_Eleven.java
+++ Divisible_By_Eleven.java
@@ -1,17 +1,19 @@
-public class Divisible_By_Eleven{
-public static int divisible(String S){
-int num = 0;
- for(int i=0; i<S.length(); i++) { 
-num = (num*10)+(S.charAt(i)-'0');
-num = num % 11;
- } 
-if(num==0){
- return 1;
- }
- return 0;
-}
-  public static void main(String[] args){
+public class Divisible_By_Eleven {
+  public static int divisible(String S) {
+    int num = 0;
+    for (int i = 0; i < S.length(); i++) {
+      num = (num * 10) + (S.charAt(i) - '0');
+      num = num % 11;
+    }
+    if (num == 0) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  public static void main(String[] args) {
     String s = "4564";
-    System.out.println(divisible (s);
+    System.out.println(divisible(s));
   }
 }

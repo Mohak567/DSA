d709494407f09f1c5841ad98bf9c39d33a0da4cf
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-09-04 20:56:37 +0530

    basic logic for string (BINARY)

--- Divisible_By_num.java
+++ Divisible_By_num.java
@@ -1,6 +1,6 @@
 public class Divisible_By_num {
     public static void main(String[] args) {
-        String binary_num = "10110";// 22
+        String binary_num = "101100";// 22
         int rem = 0;
         int num = 11;
         for (int i = 0; i < binary_num.length(); i++) {

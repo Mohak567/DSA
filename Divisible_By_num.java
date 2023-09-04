bd68c47e3d63e854784d3eeebe718f6528db40e0
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-09-04 20:53:27 +0530

    basic logic for string

---
+++ Divisible_By_num.java
@@ -0,0 +1,11 @@
+public class Divisible_By_num {
+    public static void main(String[] args) {
+        String binary_num = "10110";// 22
+        int rem = 0;
+        int num = 11;
+        for (int i = 0; i < binary_num.length(); i++) {
+            rem = (rem * 2 + (binary_num.charAt(i) - '0')) % num;
+        }
+        System.out.println(rem == 0 ? "Divisible" : "Not Divisble");
+    }
+}

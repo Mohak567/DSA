cac48d14cd4edabfad5f864d12659445d1d32abe
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-10-14 19:33:50 +0530

    logic

---
+++ Reverse_The_LinkedList.java
@@ -0,0 +1,13 @@
+public class Reverse_The_LinkedList {
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

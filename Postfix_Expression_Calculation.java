b5f3af5b6f0483af51c800d70f7659e5f9c9eacc
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-10-16 18:19:36 +0530

    using stack

---
+++ Postfix_Expression_Calculation.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class Postfix_Expression_Calculation {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        String str = "3897*/+";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isDigit(ch)) {
+                int temp = ch - '0';
+                s.push(temp);
+            } else {
+                if (ch == '/') {
+                    int num1 = s.pop();
+                    int num2 = s.pop();
+                    int ans = num1 / num2;
+                    s.push(ans);
+                } else if (ch == '*') {
+                    int num1 = s.pop();
+                    int num2 = s.pop();
+                    int ans = num1 * num2;
+                    s.push(ans);
+                } else if (ch == '+') {
+                    int num1 = s.pop();
+                    int num2 = s.pop();
+                    int ans = num1 + num2;
+                    s.push(ans);
+                } else if (ch == '-') {
+                    int num1 = s.pop();
+                    int num2 = s.pop();
+                    int ans = num1 - num2;
+                    s.push(ans);
+                }
+            }
+        }
+        System.out.println(s.peek());
+    }
+}

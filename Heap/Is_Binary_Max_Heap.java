982df34017f4aabb709f2fd889b2c95801294030
Mohak567 <138304219+Mohak567@users.noreply.github.com>
2023-10-25 18:51:33 +0530

    max/min heap

---
+++ Heap/Is_Binary_Max_Heap.java
@@ -0,0 +1,37 @@
+package Heap;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Is_Binary_Max_Heap {
+
+    public static boolean isMinHeap(TreeNode root) {
+        return helper(root, Integer.MAX_VALUE);
+    }
+
+    static boolean helper(TreeNode node, int parentValue) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val > parentValue) {
+            return false;
+        }
+        return helper(node.left, node.val) && helper(node.right, node.val);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(50);
+        root.left = new TreeNode(40);
+        root.right = new TreeNode(30);
+        root.left.left = new TreeNode(20);
+        root.left.right = new TreeNode(10);
+        System.out.println(isMinHeap(root));
+    }
+}
